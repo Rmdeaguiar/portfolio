8d2061182eb3e43c381ac6d44a7ede0bb2cfcfb0
Rafael <rafael.aguiar91@gmail.com>
2023-09-01 16:34:40 -0300

    feat: implementação de tradução para o inglês concluída

--- src/Components/Experiences/index.tsx
+++ src/Components/Experiences/index.tsx
@@ -1,31 +1,31 @@
 import './styles.css';
 import Cubos from '../../assets/cubos.png'
 import Analysis from '../../assets/analysis.png'
+import { Props } from '../../types/props';
 
-function Experiences() {
+function Experiences({english}: Props) {
 
   return (
     <div className="container-experiences">
-      <h1>Experiências</h1>
+      <h1>{english ? 'Experiences' : 'Experiências'}</h1>
       <div className='all-experiences'>
         <div className='experience'>
           <div className='local-experience'>
             <img src={Cubos} alt='Cubos' />
-            <h2>Residência Software Cubos Academy</h2>
+            <h2>{english ? 'Cubos Academy' : 'Residência Software Cubos Academy'}</h2>
           </div>
-          <h2>Desenvolvedor Full Stack</h2>
-          <h3>Janeiro/2023 - Abril/2023</h3>
-          <p>Desenvolvimento web de aplicações utilizando React Js, JavaScript, Node
-            Js, PostgreSQL, Cypress.io e Git, seguindo a Metodologia Scrum</p>
+          <h2>{english ? 'Full Stack Software Developer' : 'Desenvolvedor Full Stack'}</h2>
+          <h3>{english ? 'January/2023 - April/2023' : 'Janeiro/2023 - Abril/2023'}</h3>
+          <p>{english ? 'Web Developing applications using React Js, JavaScript, Node Js, PostgreSQL, Styled-Components, Git, API Rest, Cypress.io' : 'Desenvolvimento web de aplicações utilizando React Js, JavaScript, Node Js, PostgreSQL, Cypress.io e Git, seguindo a Metodologia Scrum'}</p>
         </div>
         <div className='experience'>
           <div className='local-experience'>
             <img src={Analysis} alt='analysisbi' />
             <h2>Analysis BI</h2>
           </div>
-          <h2>Analista de Sistema</h2>
-          <h3>Maio/2023 - Atual</h3>
-          <p>Desenvolvimento de aplicação utilizando .NET, C#, JavaScript, SQL, Git</p>
+          <h2>{english ? 'System Analyst' : 'Analista de Sistema'}</h2>
+          <h3>{english ? 'May/2023 - Now' : 'Maio/2023 - Atual'}</h3>
+          <p>{english ? 'Developing applications using C#, .NET, SQL Server, JavaScript Git' : 'Desenvolvimento de aplicação utilizando .NET, C#, JavaScript, SQL, Git'}</p>
         </div>
       </div>
 
--- src/Components/Projects/index.tsx
+++ src/Components/Projects/index.tsx
@@ -10,26 +10,26 @@ import Link from '../../assets/link.svg'
 import GitHub from '../../assets/github.svg'
 import Styled from '../../assets/styledcomp.svg'
 import Redux from '../../assets/redux.svg'
+import { Props } from '../../types/props';
 
-function Projects() {
+function Projects({ english }: Props) {
 
   return (
     <div className="container-projects">
-      <h1>Projetos</h1>
+      <h1>{english ? 'Projects' : 'Projetos'}</h1>
       <div className='projects'>
         <section>
           <div className='project'>
             <div className='content-project'>
-              <h2>Desafio MKS Sistemas</h2>
+              <h2>{english ? 'Challenge from MK Sistemas' : 'Desafio MKS Sistemas'}</h2>
               <div className='description'>
-                <p>Desafio para construção de um site para loja, onde é possível adicionar itens ao carrinho e gerenciar suas quantidades. Os produtos são obtidos através de uma api, e o design foi feito a partir de um figma já pronto.</p>
-                <p>Foi solicitada a utilização do React com TypeScript, Styled-Components e Redux</p>
+                <p>{english ? 'Challenge to build a website for a store, where you can add items to your cart and manage your requirements. The products are obtained through an API, and the design was made from a ready-made figure.' : 'Desafio para construção de um site para loja, onde é possível adicionar itens ao carrinho e gerenciar suas quantidades. Os produtos são obtidos através de uma api, e o design foi feito a partir de um figma já pronto.'}</p>
+                <p>{english ? 'Requested to use React with TypeScript, Styled-Components and Redux' : 'Foi solicitada a utilização do React com TypeScript, Styled-Components e Redux'}</p>
               </div>
             </div>
             <div>
               <div className='links-project'>
-                <a href='https://github.com/Rmdeaguiar/mkt-desafio' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />Código</a>
-                <a href='https://mks-desafio.netlify.app/' target="_blank" rel='noreferrer'><img src={Link} alt='link' />Link projeto</a>
+                <a href='https://github.com/Rmdeaguiar/mkt-desafio' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />{english ? 'Code' : 'Código'}</a>
               </div>
               <div className='languages-project'>
                 <img src={ReactJs} alt='reactjs' />
@@ -46,13 +46,13 @@ function Projects() {
             <div className='content-project'>
               <h2>Timer</h2>
               <div className='description'>
-                <p>Crônometro para marcar o tempo realizado com React Js e CSS.</p>
+                <p>{english ? 'Stopwatch to mark the time made with React Js and CSS.' : 'Crônometro para marcar o tempo realizado com React Js e CSS.'}</p>
               </div>
             </div>
             <div>
               <div className='links-project'>
-                <a href='https://github.com/Rmdeaguiar/timer' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />Código</a>
-                <a href='https://stopwatch-timer-60.netlify.app/' target="_blank" rel='noreferrer'><img src={Link} alt='link' />Link projeto</a>
+                <a href='https://github.com/Rmdeaguiar/timer' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />{english ? 'Code' : 'Código'}</a>
+                <a href='https://stopwatch-timer-60.netlify.app/' target="_blank" rel='noreferrer'><img src={Link} alt='link' />{english ? 'Project' : 'Projeto'}</a>
               </div>
               <div className='languages-project'>
                 <img src={ReactJs} alt='reactjs' />
@@ -67,13 +67,13 @@ function Projects() {
             <div className='content-project'>
               <h2>Vitor Silveira Adv.</h2>
               <div className='description'>
-                <p>Site profissional realizado para o advogado Vitor Silveira.</p>
+                <p>{english ? 'Professional website created for the lawyer Vitor Silveira.' : 'Site profissional realizado para o advogado Vitor Silveira.'}</p>
               </div>
             </div>
             <div>
               <div className='links-project'>
-                <a href='https://github.com/Rmdeaguiar/vitorcsadv' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />Código</a>
-                <a href='https://www.vitorsilveiraadv.com.br/' target="_blank" rel='noreferrer'><img src={Link} alt='link' />Link projeto</a>
+                <a href='https://github.com/Rmdeaguiar/vitorcsadv' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />{english ? 'Code' : 'Código'}</a>
+                <a href='https://www.vitorsilveiraadv.com.br/' target="_blank" rel='noreferrer'><img src={Link} alt='link' />{english ? 'Project' : 'Projeto'}</a>
               </div>
               <div className='languages-project'>
                 <img src={ReactJs} alt='reactjs' />
@@ -86,16 +86,16 @@ function Projects() {
         <section>
           <div className='project'>
             <div className='content-project'>
-              <h2>Lista de Tarefas</h2>
+              <h2>{english ? 'To Do List' : 'Lista de Tarefas'}</h2>
               <div className='description'>
-                <p>Uma aplicação para organização de tarefas, permitindo ao usuário se cadastrar, logar e começar a gerenciar suas tarefas à serem feitas.</p>
+                <p>{english ? 'An application for organizing tasks, allowing the user to register, log in and start managing their tasks to be done.' : 'Uma aplicação para organização de tarefas, permitindo ao usuário se cadastrar, logar e começar a gerenciar suas tarefas à serem feitas.'}</p>
               </div>
             </div>
             <div>
               <div className='links-project'>
                 <a href='https://github.com/Rmdeaguiar/front-todolist' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />Front-End</a>
                 <a href='https://github.com/Rmdeaguiar/back-todolist' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />Back-End</a>
-                <a href='https://sualistadetarefas.netlify.app/' target="_blank" rel='noreferrer'><img src={Link} alt='link' />Link projeto</a>
+                <a href='https://sualistadetarefas.netlify.app/' target="_blank" rel='noreferrer'><img src={Link} alt='link' />{english ? 'Project' : 'Projeto'}</a>
               </div>
               <div className='languages-project'>
                 <img src={ReactJs} alt='reactjs' />
@@ -114,18 +114,21 @@ function Projects() {
             <div className='content-project'>
               <h2>Payment Management</h2>
               <div className='description'>
-                <p>Projeto final de conclusão do curso de Desenvolvimento de Software da Cubos Academy.
-                  Foi realizada uma aplicação onde usuários de uma organização poderiam cadastrar, editar ou remover clientes e suas respectivas cobranças.</p>
-                  <p>
-                  Além disso, algumas funcionalidades como resumo total de cobranças, pesquisa, ordenação e detalhamento de cada cliente/cobrança também estão disponíveis para os usuários da aplicação. O projeto foi realizado por uma equipe de 5 pessoas.</p>
-                  
+                <p>{english ? `An application was created where users of an organization could register, edit or remove customers and their respective charges. In 
+addition, some functionalities such as a summary of total charges, searching, sorting and detailing of each customer/charge are also 
+available to the application's users. This project was made by a team of five people, and were used the following technologies: 
+JavaScript, PostgreSQL, Node Js (Back-End) and React Js, CSS (Front-End). The Scrum Methodology for organizing and Git code 
+versioning control were also used. ` : 'Projeto final de conclusão do curso de Desenvolvimento de Software da Cubos Academy. Foi realizada uma aplicação onde usuários de uma organização poderiam cadastrar, editar ou remover clientes e suas respectivas cobranças.'}</p>
+                <p>{english ? `In addition, some functionalities such as a summary of total charges, search, sorting and detailing of each customer/charge are also available to the application's users. The project was carried out by a team of 5 people.` :
+                  'Além disso, algumas funcionalidades como resumo total de cobranças, pesquisa, ordenação e detalhamento de cada cliente/cobrança também estão disponíveis para os usuários da aplicação. O projeto foi realizado por uma equipe de 5 pessoas.'}</p>
+
               </div>
             </div>
             <div>
               <div className='links-project'>
                 <a href='https://github.com/Rmdeaguiar/payment-management-front' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />Front-End</a>
                 <a href='https://github.com/Rmdeaguiar/payment-management-back' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />Back-End</a>
-                <a href='https://payment-management.netlify.app/' target="_blank" rel='noreferrer'><img src={Link} alt='link' />Link projeto</a>
+                <a href='https://payment-management.netlify.app/' target="_blank" rel='noreferrer'><img src={Link} alt='link' />{english ? 'Project' : 'Projeto'}</a>
               </div>
               <div className='languages-project'>
                 <img src={ReactJs} alt='reactjs' />
@@ -144,7 +147,7 @@ function Projects() {
             <div className='content-project'>
               <h2>NG App</h2>
               <div className='description'>
-                <p>Aplicação construída para o Desafio NG. Uma aplicação simulando um banco, onde o usuário pode se cadastrar, entrar e ter acesso a algumas funcionalidades, como visualização do seu balanço, de suas transferências, e também para realizar uma nova transação bancária.</p>
+                <p>{english ?'Application built for the NG Challenge. An application simulating a bank, where the user can register, enter and have access to some functionalities, such as viewing his balance sheet, transfers, and also to carry out a new bank transaction.' : 'Aplicação construída para o Desafio NG. Uma aplicação simulando um banco, onde o usuário pode se cadastrar, entrar e ter acesso a algumas funcionalidades, como visualização do seu balanço, de suas transferências, e também para realizar uma nova transação bancária.'}</p>
               </div>
             </div>
             <div>
@@ -169,13 +172,13 @@ function Projects() {
             <div className='content-project'>
               <h2>Atelier Denise Colaferro</h2>
               <div className='description'>
-              <p>Aplicação construída para a artista Denise Colaferro. Um site onde clientes podem visualizar as peças disponíveis e entrar em contato diretamente pelo WhatsApp ou Instagram.</p>
-            </div>
+                <p>{english ? 'Application built for the artist Denise Colaferro. A website where customers can view available pieces and get in touch directly via WhatsApp or Instagram.' : 'Aplicação construída para a artista Denise Colaferro. Um site onde clientes podem visualizar as peças disponíveis e entrar em contato diretamente pelo WhatsApp ou Instagram.'}</p>
+              </div>
             </div>
             <div>
               <div className='links-project'>
-                <a href='https://github.com/Rmdeaguiar/atelierdenisecolaferro' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />Código</a>
-                <a href='https://atelierdenisecolaferro.netlify.app/' target="_blank" rel='noreferrer'><img src={Link} alt='link' />Link projeto</a>
+                <a href='https://github.com/Rmdeaguiar/atelierdenisecolaferro' target="_blank" rel='noreferrer'><img src={GitHub} alt='github' />{english ? 'Code' : 'Código'}</a>
+                <a href='https://atelierdenisecolaferro.netlify.app/' target="_blank" rel='noreferrer'><img src={Link} alt='link' />{english ? 'Project' : 'Projeto'}</a>
               </div>
               <div className='languages-project'>
                 <img src={ReactJs} alt='reactjs' />
